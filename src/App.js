8b28bd3e10799aaf9757ebda243843a675dfd0c0
NAVANEETHV9388 <41069175+vnavaneeth10@users.noreply.github.com>
2022-01-31 12:16:47 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
+import React, { Component } from 'react';
+import{
+  BrowserRouter as Router,
+  Route,
+}from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesList from './pages/ArticlesList';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+
+  render() {
+    
+    return (
+    <Router>
     <div className="App">
+      <div id="page-body">
+      <Route path="/" component ={HomePage} exact />
+      <Route path="/about" component={AboutPage} />
+      <Route path="/articles-list" component={ArticlesList} />
+      <Route path="/article" component={ArticlePage} />
       <HomePage/>
     </div>
+    </div>
+    </Router>  
+  
   );
+  
+ }
+
 }
 
 export default App;
